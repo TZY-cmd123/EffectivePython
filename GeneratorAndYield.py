3430e958825a1e41eda4edd57fce8c259a6a6ae7
TZY <1137721268@qq.com>
2021-01-02 15:34:22 +0800

    生成器用处和好处

--- GeneratorAndYield.py
+++ GeneratorAndYield.py
@@ -22,21 +22,52 @@ except TypeError as e:
 
 
 yi=100000000
-time1=time.time()
-mem1=mem.memory_usage()
-# 数量变大时,使用列表
-nums1=list(range(yi))
-squre_nums1=[n**3 for n in nums1]
-time2=time.time()
-mem2=mem.memory_usage()
-print(f"时间使用{time2-time1}")
-print(f"内存使用{mem2[0]-mem1[0]}")
-# 使用迭代器
-nums2=list(range(yi))
-squre_nums2=(n**3 for n in nums2)
-time3=time.time()
-mem3=mem.memory_usage()
-print(f"时间使用{time3-time2}")
-print(f"内存使用{mem3[0]-mem2[0]}")
+# time1=time.time()
+# mem1=mem.memory_usage()
+# # 数量变大时,使用列表
+# nums1=list(range(yi))
+# squre_nums1=[n**3 for n in nums1]
+# time2=time.time()
+# mem2=mem.memory_usage()
+# print(f"时间使用{time2-time1}")
+# print(f"内存使用{mem2[0]-mem1[0]}")
+# # 使用迭代器
+# nums2=list(range(yi))
+# squre_nums2=(n**3 for n in nums2)
+# time3=time.time()
+# mem3=mem.memory_usage()
+# print(f"时间使用{time3-time2}")
+# print(f"内存使用{mem3[0]-mem2[0]}")
 
-# 所以列表中的数据是真的生成了,而在生成器中只是生成了生成器,每当需要数字的时候调用这个生成器
+# 所以列表中的数据是真的生成了,而在生成器中只是生成了生成器,每当需要数字的时候调用这个生成器,而不是一次性生成完
+
+# 比较正规的写法是用yield来制作生成器
+def calc_nums(nums):
+    new_nums = []
+    for n in range(nums):
+        if n%3==0:
+            new_nums.append(3*yi)
+        elif n %5==0:
+            new_nums.append(5*yi)
+        else:
+            new_nums.append(n*yi)
+    return new_nums #正常的写法，直接把数字发过来
+
+
+
+def gen_nums(nums):
+    for n in range(nums):
+        if n%3==0:
+            yield 3*yi
+        elif n %5==0:
+            yield 5*yi
+        else:
+            yield n*yi #调用生成器,每当生成的结果调用next时，就会重新调用这个函数
+
+
+cnums=calc_nums(10)
+gnums=gen_nums(10)  # 后者是迭代器
+for i in  cnums:
+    print(i)
+for i in gnums:
+    print(i)
